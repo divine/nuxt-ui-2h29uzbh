8cd5218b189a0a175925603e1b0630180fab4660
Divine <48183131+divine@users.noreply.github.com>
2024-12-08 23:31:06 +0300

    feat: add nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui-pro'],
+  compatibilityDate: '2024-12-08',
 });
